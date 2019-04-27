da70a1c0c51e4378af3aefa169cab4a9c31ae2e3
emilerik <emil.eriksson69@gmail.com>
2019-04-27 16:45:28 +0200

    update

--- src/clarifai/image.js
+++ src/clarifai/image.js
@@ -1,29 +1,28 @@
-//const APIKeys = require("./API-keys.js");
 const Clarifai = require("clarifai");
 
 const app = new Clarifai.App({
-  apiKey: process.env.API_CLARIFAI
+  //apiKey: process.env.API_CLARIFAI
+  apiKey: "744e8eb3e84c49cb9ea1e0fe79fc46c4"
 });
 
 const handleApiCall = (req, res) => {
-  const url = mapimgToURL(req.body);
+  const url = images[req.body.input];
   app.models
     .predict(Clarifai.COLOR_MODEL, url)
     .then(data => {
-      res.json(data.outputs[0].data.colors);
+      const color = data.outputs[0].data.colors[0].raw_hex;
+      if (color === "#007efe") {
+        res.end("G Flat");
+      } else {
+        res.end("F");
+      }
     })
-    .catch(err => res.status(400).json("unable to work with API. "));
-};
-
-const mapimgToURL = img => {
-  return images[img];
+    .catch(err => res.status(400).json("unable to work with API. " + err));
 };
 
 const images = {
-  image1:
-    "http://andb.info/wp-content/uploads/2018/11/blue-chair-blessing-lounge-blue-chair-blessing-images.jpg",
-  image2:
-    "https://www.dhresource.com/0x0s/f2-albu-g4-M01-E0-7B-rBVaEVgWspSAJaWGAACxt1ZQ9rs536.jpg/wholesale-minimalist-bedding-sets-apple-green.jpg"
+  image1: "https://upload.wikimedia.org/wikipedia/commons/e/e4/Color-blue.JPG",
+  image2: "https://www.tascsoftware.co.uk/wiki/INSIGHT/images/0/0e/Green.jpg"
 };
 
 module.exports = {
