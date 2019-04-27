846d0db231fc855bcec6d9c07514f9d700f7f1db
emilerik <emil.eriksson69@gmail.com>
2019-04-27 14:46:01 +0200

    created a server and image endpoint

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+//Controllers
+const image = require("./image");
+
+const app = express();
+
+//Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+//End-points
+app.get("/", (req, res) => {
+  res.send("This is the ColorChecker server");
+});
+app.post("/imageurl", (req, res) => image.handleApiCall(req, res));
+
+app.listen(3000, () => {
+  console.log(`app is running on port 3000`);
+});
